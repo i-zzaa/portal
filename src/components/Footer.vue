8e6944fcc5a35b7c5a74808732e3dc9becdba7ed
c1321644 Isa Andressa Novaes Menezes <c1321644@interno.bb.com.br>
2023-06-01 21:49:33 -0300

    feat: estrutura

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,41 +1,28 @@
 <template>
-  <footer class="bg-blue-02">
-    <ul class="grid grid-cols-3 h-full w-full text-white">
+  <footer class="bg-primary">
+    <ul class="flex justify-between w-full text-white px-4 py-2">
       <li class="flex items-center gap-2 justify-center">
-        <span class="leading-4 flex gap-2 items-center"> MENU PRINCIPAIS </span>
+        <span class="leading-4 flex gap-2 items-center text-xs">
+          <ph-map-pin :size="10" weight="bold" class="duration-700" />
+          {{ $t("footer_location") }}</span
+        >
       </li>
       <li class="grid items-center leading-4">
-        <span>CENTRAL DE ATENDIMENTO </span>
         <span class="text-xs leading-4 flex gap-2 items-center">
-          <ph-map-pin
-            :size="10"
-            weight="bold"
-            class="duration-700 hover:cursor-pointer hover:scale-110"
-          />
-          Prodam - Empresa de Tecnologia da Informação e Comunicação do
-          Município de São Paulo</span
-        >
-        <span class="text-xs leading-4 flex gap-2 items-center">
-          <ph-phone
-            :size="10"
-            weight="bold"
-            class="duration-700 hover:cursor-pointer hover:scale-110"
-          />
+          <ph-phone :size="10" weight="bold" class="duration-700" />
 
-          Telefone: 61 3246-6340
+          {{ $t("footer_tel") }}
         </span>
+      </li>
+      <li class="grid items-center leading-4">
         <span class="text-xs leading-4 flex gap-2 items-center">
-          <ph-at
-            :size="10"
-            weight="bold"
-            class="duration-700 hover:cursor-pointer hover:scale-110"
-          />
-          atendimento-prodam@ios.com.br
+          <ph-at :size="10" weight="bold" class="duration-700" />
+          {{ $t("footer_email") }}
         </span>
       </li>
-      <li class="p-2">
+      <!-- <li class="p-2">
         <div class="bg-logo-02 bg-no-repeat bg-contain h-24 w-48"></div>
-      </li>
+      </li> -->
     </ul>
   </footer>
 </template>
