92b8e21aed44e19d78b57a3b69b7d6c5b747fcf8
c1321644 Isa Andressa Novaes Menezes <c1321644@interno.bb.com.br>
2023-06-06 22:37:08 -0300

    feat: auth jwt

--- src/router/index.ts
+++ src/router/index.ts
@@ -46,7 +46,7 @@ const router = createRouter({
   ],
 });
 
-router.beforeEach((to, from, next) => {
+router.beforeEach((to, _, next) => {
   const store = useAuth();
 
   const loggedIn = store.isLoggedIn;
