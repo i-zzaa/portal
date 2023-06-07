d41b6849a5cac6ec00d25592c999d90e9472560b
c1321644 Isa Andressa Novaes Menezes <c1321644@interno.bb.com.br>
2023-06-07 20:20:20 -0300

    feat:  ajustes

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="bg-white dark:bg-slate-800 bg-clip-borde border-l-8 w-full rounded-lg px-6 py-6 ring-1 ring-slate-900/5 hover:shadow-lg hover:cursor-pointer hover:scale-110 duration-700 grid"
+    class="bg-white bg-clip-borde border-l-8 w-full rounded-lg px-6 py-6 ring-1 ring-slate-900/5 hover:shadow-lg hover:cursor-pointer hover:scale-110 duration-700 grid"
     :class="color"
   >
     <slot></slot>
